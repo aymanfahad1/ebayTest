3ffbbd4a21bd2b9346671346f1a8b5df73840bf6
Ayman <aymanfahad427@gmail.com>
2023-03-03 00:18:56 +0600

    Updated the browser control

--- src/test/java/ebay/testCase/baseTest.java
+++ src/test/java/ebay/testCase/baseTest.java
@@ -1,7 +1,13 @@
 package ebay.testCase;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -12,16 +18,34 @@ public class baseTest {
 	public WebDriver driver;
 	public landOnPage landOnPage;
 	
-	public void startTheBrowser()
+	public WebDriver startTheBrowser() throws IOException
 	{
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
+		 Properties prop =new Properties();
+		 FileInputStream fis = new FileInputStream("C:\\Users\\rashi\\eclipse-workspace\\ebay\\src\\main\\java\\ebay\\resources\\browserName.properties");
+		 prop.load(fis);
+		 
+		 String browserName = prop.getProperty("browser");
+		 if(browserName.equalsIgnoreCase("chrome"))
+		 {
+			 driver = new ChromeDriver();
+		 }
+		 else if(browserName.equalsIgnoreCase("firefox"))
+		 {
+			 driver = new FirefoxDriver();
+		 }
+		 else if(browserName.equalsIgnoreCase("edge"))
+		 {
+			 driver = new EdgeDriver();
+		 }
+		 driver.manage().window().maximize();
+		 return driver;
+		
 	}
 	
 	@BeforeMethod(alwaysRun = true)
-	public ebay.pageObjects.landOnPage landOnPage ()
+	public ebay.pageObjects.landOnPage landOnPage () throws IOException
 	{
-		startTheBrowser();
+		driver = startTheBrowser();
 		landOnPage = new landOnPage(driver);
 		landOnPage.ebayWebsite();
 		return landOnPage;
